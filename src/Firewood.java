475bfc3269794bf1bf914c5de9bb6ca965e443d5
Nese <neseilhannn@gmail.com>
2024-05-21 00:40:18 +0300

    Firewood, Water and Food classes added.

---
+++ src/Firewood.java
@@ -0,0 +1,15 @@
+public class Firewood {
+    private String name;
+
+    public Firewood(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/Water.java
@@ -0,0 +1,15 @@
+public class Water {
+    private String name;
+
+    public Water(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
